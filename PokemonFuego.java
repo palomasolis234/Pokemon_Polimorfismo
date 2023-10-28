9ee32074d7db638eba45f8d18152da93855e2bbd
palomasolis234 <132014943+palomasolis234@users.noreply.github.com>
2023-10-27 23:59:12 -0600

    Update PokemonFuego.java

--- PokemonFuego.java
+++ PokemonFuego.java
@@ -1,5 +1,50 @@
-package BatallaPokemon;
+import pokemon.ListaMovimientos;
 
-public class PokemonFuego {
+public class PokemonFuego extends Pokemon{
+    Pokemon pokemon;
+    public PokemonFuego(String nombre, int nivel) {
+        super(nombre, Tipo.FUEGO, nivel);
+
+        ListaMovimientos listaMovimientos = new ListaMovimientos();
+
+        setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Ascuas Ember"));
+        setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Colmillo Ígneo Fire Fang"));
+        setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Lanzallamas Flamethrower"));
+        setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Buena Baza Assurance"));
+        setMovimientos(4, listaMovimientos.buscarMovimientoPorNombre("Giro Fuego Fire Spin"));
+        
+}
+/**
+ * @param pokemon
+ * @return
+ */
+public double obtenerEfectividad(Pokemon pokemon){
+    double efectividad = 1.0;
+
+    if(pokemon.getTipo()== Tipo.PLANTA) efectividad = 2.0;
+    if(pokemon.getTipo()== Tipo.FUEGO) efectividad = 0.5;
+    if(pokemon.getTipo()== Tipo.ROCA) efectividad = 0.5;
+    if(pokemon.getTipo()== Tipo.HIELO) efectividad = 2.0;
+    if(pokemon.getTipo()== Tipo.ELECTRICO) efectividad = 0.5;
+    
+return efectividad;
+
+public double CalcularDanio(Pokemon pokemon){
+    
+    calculaDanio(int danio) 
+    recibirAtaque(String movimiento)
+     atacar(String movimiento, Pokemon pokemon)
+    Atacar();
+    int Defensa(); 
+    intAtaque especia();
+     int Defensa especial(); 
+    int Velocidad();
+    int Nivel(); 
+    
+
+
+
 
 }
+}
+    
